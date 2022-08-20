2791dd20e4d87d0421bad29876d934d29c249274
Andres Martinez <amartinezcarrillo1006@gmail.com>
2022-08-20 14:54:49 -0300

    Agregue un nav-bar mediante jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import BarraNavegacion from './components/NavBar';
 function App() {
   return (
     <div className="App">
+         <BarraNavegacion />
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Andres Martinez
-        </p>
+        <p>Andres Martinez</p>
         <p>On Cases Store</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a className="App-link"href="https://reactjs.org"target="_blank"rel="noopener noreferrer">Learn React</a>
       </header>
     </div>
   );
