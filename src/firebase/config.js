c6c81d7811bef4939294544984611d548f0aad1c
Andres Martinez <amartinezcarrillo1006@gmail.com>
2022-09-26 20:17:41 -0300

    Agregue firebase al proyecto

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA5QVnPgb9282DLzsWBjnc4-yGL3ef81q8",
+  authDomain: "on-cases-store.firebaseapp.com",
+  projectId: "on-cases-store",
+  storageBucket: "on-cases-store.appspot.com",
+  messagingSenderId: "730250822872",
+  appId: "1:730250822872:web:eb8f923469fc455c6083ad",
+  measurementId: "G-Z0LZB3ETET"
+};
+
+// Initialize Firebase
+ initializeApp(firebaseConfig);
